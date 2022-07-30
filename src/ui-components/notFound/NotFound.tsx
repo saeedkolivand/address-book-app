d5b06a13a45d7e2e50e30b366a74d314a9341e2d
Saeed Kolivand <saeedkolivand1997@gmail.com>
2022-07-30 20:11:56 +0430

    fix: modify global font, edit Button component styles, add Back to Home to NotFound component

--- src/ui-components/notFound/NotFound.tsx
+++ src/ui-components/notFound/NotFound.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { NotFoundPropsTypes } from "./notFound.types";
 import "./notFound.style.scss";
+import { Link } from "react-router-dom";
+import { HomePathNames } from "pages/home/home.route";
+import Button from "../button/Button";
+import { NotFoundPropsTypes } from "./notFound.types";
 
 const NotFound: React.FC<NotFoundPropsTypes> = (props) => {
   const { className = "" } = props;
 
   return (
     <div
-      className={`not-found-wrapper absolute-center ${className}`}
+      className={`not-found-wrapper absolute-center flex-center ${className}`}
       aria-label="not-found-wrapper"
     >
       Page Not Found :(
+      <Link to={HomePathNames.root} className="not-found-wrapper__back-button">
+        <Button>Back to Home</Button>
+      </Link>
     </div>
   );
 };
