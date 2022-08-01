15bd3d95f373cbd77031fb93fc4cd2cf79c22ef9
Saeed Kolivand <saeedkolivand1997@gmail.com>
2022-08-01 18:51:27 +0430

    feat: add UsersList test cases

--- src/components/usersList/usersList.test.tsx
+++ src/components/usersList/usersList.test.tsx
@@ -2,7 +2,7 @@ import { cleanup, render, screen } from "@testing-library/react";
 import UsersList from "./UsersList";
 import { mockedApiResponse } from "../../../__mocks__/apiResponse";
 
-describe("RepositoryList test cases", () => {
+describe("UsersList test cases", () => {
   afterAll(cleanup);
 
   it("should render two items in list", () => {
@@ -26,5 +26,32 @@ describe("RepositoryList test cases", () => {
     const repositoryElement = screen.getAllByLabelText("user-card-wrapper");
 
     expect(repositoryElement.length).toBe(2);
+  });
+
+  it("should pass user data to onClickItem", () => {
+    const onClickItem = jest.fn();
+
+    render(
+      <UsersList
+        usersList={mockedApiResponse}
+        headerItems={[
+          "Picture",
+          "First Name",
+          "Last Name",
+          "Username",
+          "Email",
+        ]}
+        onUpdatePageNumber={() => {}}
+        isFirstPage
+        isFilterActive
+        onClickItem={onClickItem}
+      />
+    );
+
+    const repositoryElement = screen.getAllByLabelText("user-card-wrapper");
+
+    repositoryElement[0].click();
+
+    expect(onClickItem).toHaveBeenCalled();
   });
 });
